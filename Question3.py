637cc7d8917d31e29e2360336d32421dc4e60bb6
developerjoshmutinda <160116316+developerjoshmutinda@users.noreply.github.com>
2024-02-16 12:45:31 +0300

    Update Question3.py

--- Question3.py
+++ Question3.py
@@ -1,1 +1,10 @@
+# Write a program that takes an integer as input and returns true if the input is a power of two.
+def is_power_of_two(n):
+    if n <= 0:
+        return False
+    else:
+        return (n & (n - 1)) == 0
 
+# Test cases
+print(is_power_of_two(8))  # returns True
+print(is_power_of_two(6))  # returns False
